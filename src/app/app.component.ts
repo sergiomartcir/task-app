68c3abbfbd815fce2d3bc4f41fd0c8b3dd79f143
sergiomartcir <smartinc@minsait.com>
2026-04-30 16:24:09 +0200

    feat: inicialización del proyecto y esqueleto inicial con splash, header y footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
-import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
+import { IonApp, IonRouterOutlet, IonContent } from '@ionic/angular/standalone';
+import { HeaderComponent } from "./shared/components/header/header.component";
+import { FooterComponent } from "./shared/components/footer/footer.component";
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  imports: [IonApp, IonRouterOutlet],
+  imports: [IonApp, IonRouterOutlet, HeaderComponent, IonContent, FooterComponent],
 })
 export class AppComponent {
   constructor() {}
